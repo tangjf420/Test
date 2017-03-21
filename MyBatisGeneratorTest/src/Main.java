ca91263121f092a23bbcb943a967d6fc673a34b9
tangjf <tangjf@skong.com>
2017-03-21 20:50:45 +0800

    格式化

--- MyBatisGeneratorTest/src/Main.java
+++ MyBatisGeneratorTest/src/Main.java
@@ -5,42 +5,11 @@ import java.util.List;
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
-/**
- * @Title: Main.java
- * @Package:
- * @Description: TODO
- * @author: tangjf(tangjunfeng2006@126.com)
- * @date: 2015骞�6鏈�24鏃� 涓嬪崍11:31:02
- * @version: V1.0
- */
-
-/**
- * @moudle: Main
- * @version:v1.0
- * @Description: TODO
- * @author: tangjf(tangjunfeng2006@126.com)
- * @date: 2015骞�6鏈�24鏃� 涓嬪崍11:31:02
- *
- */
 public class Main {
 
-	/**
-	 *
-	 * <p>
-	 * Title: main
-	 * </p>
-	 * <p>
-	 * author : tangjf(tangjunfeng2006@126.com)
-	 * </p>
-	 * <p>
-	 * date : 2015骞�6鏈�24鏃� 涓嬪崍11:31:02
-	 * </p>
-	 * 
-	 * @param args
-	 * @throws Exception
-	 */
 	public static void main(String[] args) {
 		try {
 			List<String> warnings = new ArrayList<String>();
@@ -53,6 +22,9 @@ public class Main {
 
 			mybatisGenerator.generate(null);
 			System.out.println("OK");
+		} catch (InvalidConfigurationException e) {
+			e.printStackTrace();
+			System.out.println(e.getErrors());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
